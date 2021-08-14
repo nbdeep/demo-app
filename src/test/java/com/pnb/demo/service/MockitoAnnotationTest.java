9f26d8ba7684fd5003e65fe531121918f157edfc
nbdeep <nbstock19@gmail.com>
2021-08-14 20:29:38 +0530

    local change

--- src/test/java/com/pnb/demo/service/MockitoAnnotationTest.java
+++ src/test/java/com/pnb/demo/service/MockitoAnnotationTest.java
@@ -48,6 +48,7 @@ class MockitoAnnotationTest {
 
         Assertions.assertThrows(IndexOutOfBoundsException.class,
                 () -> {
+                    System.out.println("test checkin");
                     System.out.println(mockList.size());
                     mockList.get(200);
                 });
