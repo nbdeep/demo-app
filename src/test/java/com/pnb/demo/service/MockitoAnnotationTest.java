a5a84a25ab16f58198551f40876aef37db31a0b7
nbdeep <nbstock19@gmail.com>
2021-08-14 20:15:07 +0530

    add

---
+++ src/test/java/com/pnb/demo/service/MockitoAnnotationTest.java
@@ -0,0 +1,54 @@
+package com.pnb.demo.service;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class MockitoAnnotationTest {
+
+    @Mock
+    private ArrayList<String> mockList;
+
+    @Spy
+    private ArrayList<String> spyList;
+
+    @Test
+     void whenNotUseMockAnnotation_thenCorrect() {
+        mockList.add("one");
+        Mockito.verify(mockList).add("one");
+        assertEquals(0, mockList.size());
+
+        Mockito.when(mockList.size()).thenReturn(100);
+        assertEquals(100, mockList.size());
+    }
+
+    @Test
+    void spyListTest() {
+        spyList.add("one");
+        assertEquals(1, spyList.size());
+        //call doReturn for Spy
+        doReturn(100).when(spyList).size();
+        assertEquals(100, spyList.size());
+    }
+
+    @Test
+    public void throw_NPE() {
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class,
+                () -> {
+                    System.out.println(mockList.size());
+                    mockList.get(200);
+                });
+    }
+}
